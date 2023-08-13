1310a7a25d422cc1c8d8dfaee808039f7cf5551a
蒋勇 <jiangy@highzap.com>
2023-08-13 15:59:47 +0800

    init project

---
+++ src/main/java/com/example/demo/controller/HomeController.java
@@ -0,0 +1,21 @@
+package com.example.demo.controller;
+
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+/**
+ * <p>创建时间: 2023/8/13 </p>
+ *
+ * @author <a href="mailto:jiangliu0316@dingtalk.com" rel="nofollow">蒋勇</a>
+ */
+@RestController
+public class HomeController {
+    @GetMapping("/")
+    public Map<String, Object> home(@AuthenticationPrincipal OAuth2User user) {
+        return user.getAttributes();
+    }
+}
